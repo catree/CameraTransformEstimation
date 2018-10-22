2442dad784ac5446ca50a0f5251f367b71957f43
Joker Smith <baidujoker@gmail.com>
2018-10-22 12:19:29 -0400

    timer.py

---
+++ timer.py
@@ -0,0 +1,15 @@
+import time
+
+class Timer(object):
+	"""docstring for Timer"""
+	def __init__(self):
+		super(Timer, self).__init__()
+		self._start = None
+
+	def tic(self):
+		self._start = time.time()
+#		return self
+
+	def toc(self):
+		assert self._start != None, 'Timer uninitialized. Call "toc()" first.'
+		return time.time() - self._start
